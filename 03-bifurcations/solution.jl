ecd682a443a89826dcb05c55c7a0ae67f535a02c
Bartosz Zbik <bartosz@zbik.org>
2024-03-17 10:20:35 +0100

    find all stable points

--- 03-bifurcations/solution.jl
+++ 03-bifurcations/solution.jl
@@ -31,26 +31,22 @@ function final_state(f, r, x0, dt, tmax)
     return sol.u[end]
 end
 
-function find_steady_states(x0_rng, xf_rng, atol)
+function find_steady_states(r, f, x0_rng, dt, tmax, atol)
+    # solves the equation to get final positions after a long time
+    xf_rng = [final_state(f, r, x0, dt, tmax) for x0 in x0_rng]
+
+    # finds neighbouring solutions that converge to different attractors
     unst_index = findall([!isapprox(xf_rng[i], xf_rng[i+1], atol=atol) for i in 1:length(x0_rng)-1])
     unstable = [0.5 * (x0_rng[i] + x0_rng[i+1]) for i in unst_index]
 
-    unst_index = [1, unst_index..., length(xf_rng)]
-    stable = [mean(xf_rng[unst_index[i]:unst_index[i+1]]) for i in 1:length(unst_index)-1]
+    border_index = [0, unst_index..., length(xf_rng)]
+    stable = [mean(xf_rng[border_index[i]+1:border_index[i+1]]) for i in 1:length(border_index)-1]
     return stable, unstable
-end
-
-function find_stable_points(r, f, x0_rng, dt, tmax, atol)
-    # solves the equation to get final positions after a long time
-    xfs = [final_state(f, r, x0, dt, tmax) for x0 in x0_rng]
-    # groups the solutions and calculates a mean in each bucket
-    stable = [mean(x) for x in groupby_numbers(xfs; atol=atol)]
-    return sort(stable)
 end
 
 
 # sol = get_sol(f, 0.0, 0.1, dt, 1e3)
-# all_stable = [find_stable_points(r, f, x0_rng, dt, 1e2, 1e-2) for r in r_rng]
+all_ss = [find_steady_states(r, f, x0_rng, dt, 2e2, 1e-2) for r in r_rng]
 
 
 using CairoMakie
@@ -58,8 +54,32 @@ using CairoMakie
 fig = Figure()
 ax = Axis(fig[1,1])
 
-for x in eachindex(all_stable)
-    scatter!(ax, fill(r_rng[x], length(all_stable[x])), all_stable[x], color=:black, markersize=3)
+for i in eachindex(all_ss)
+    stable, unstable = all_ss[i]
+    scatter!(ax, fill(r_rng[i], length(stable)), stable, color=Makie.wong_colors()[5])
+    scatter!(ax, fill(r_rng[i], length(unstable)), unstable,
+             color=Makie.wong_colors()[2], marker=:utriangle)
 end
 
+lines!(ax, -1..0, x -> 0, color=:black)
+lines!(ax, 0..1, x -> 0, color=:red, linestyle=:dash)
+
+lines!(ax, -0.25..0, r -> sqrt(0.5 - sqrt(0.25 + r)), color=:red, linestyle=:dash)
+lines!(ax, -0.25..0, r -> -sqrt(0.5 - sqrt(0.25 + r)), color=:red, linestyle=:dash)
+
+lines!(ax, -0.25..1, r -> sqrt(0.5 + sqrt(0.25 + r)), color=:black)
+lines!(ax, -0.25..1, r -> -sqrt(0.5 + sqrt(0.25 + r)), color=:black)
+
+ax.xlabel = L"$r$ - parameter"
+ax.ylabel = L"$x$ - stable point position"
+
+elem_1 = [LineElement(color = :black, linestyle = nothing)]
+elem_2 = [LineElement(color = :red, linestyle = :dash)]
+elem_3 = [MarkerElement(color = Makie.wong_colors()[5], marker = :circle)]
+elem_4 = [MarkerElement(color = Makie.wong_colors()[2], marker = :utriangle)]
+axislegend(ax, [elem_1, elem_2, elem_3, elem_4],
+           ["theoretical stable", "theoretical unstable", "numerical stable", "numerical unstable"],
+          position=:lt)
+
+save("bifurc-diagram.pdf", fig)
 
