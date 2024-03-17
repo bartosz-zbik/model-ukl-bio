a97e9564db87efb071d35a749516de7698fbc10b
Bartosz Zbik <bartosz@zbik.org>
2024-03-17 11:37:19 +0100

    final report version

--- 03-bifurcations/make_plots.jl
+++ 03-bifurcations/make_plots.jl
@@ -6,27 +6,27 @@ ax = Axis(fig[1,1])
 
 for i in eachindex(all_ss)
     stable, unstable = all_ss[i]
-    scatter!(ax, fill(r_rng[i], length(stable)), stable, color=Makie.wong_colors()[5])
+    scatter!(ax, fill(r_rng[i], length(stable)), stable, color=Makie.wong_colors()[5], alpha=0.6)
     scatter!(ax, fill(r_rng[i], length(unstable)), unstable,
-             color=Makie.wong_colors()[2], marker=:utriangle)
+             color=Makie.wong_colors()[2], marker=:utriangle, alpha=0.6)
 end
 
 lines!(ax, -1..0, x -> 0, color=:black)
-lines!(ax, 0..1, x -> 0, color=:red, linestyle=:dash)
+lines!(ax, 0..1, x -> 0, color=:black, linestyle=:dash)
 
-lines!(ax, -0.25..0, r -> sqrt(0.5 - sqrt(0.25 + r)), color=:red, linestyle=:dash)
-lines!(ax, -0.25..0, r -> -sqrt(0.5 - sqrt(0.25 + r)), color=:red, linestyle=:dash)
+lines!(ax, -0.25..0, r -> sqrt(0.5 - sqrt(0.25 + r)), color=:black, linestyle=:dash)
+lines!(ax, -0.25..0, r -> -sqrt(0.5 - sqrt(0.25 + r)), color=:black, linestyle=:dash)
 
 lines!(ax, -0.25..1, r -> sqrt(0.5 + sqrt(0.25 + r)), color=:black)
 lines!(ax, -0.25..1, r -> -sqrt(0.5 + sqrt(0.25 + r)), color=:black)
 
 ax.xlabel = L"$r$ - parameter"
-ax.ylabel = L"$x$ - stable point position"
+ax.ylabel = L"$x$ - steady state position"
 
 elem_1 = [LineElement(color = :black, linestyle = nothing)]
-elem_2 = [LineElement(color = :red, linestyle = :dash)]
-elem_3 = [MarkerElement(color = Makie.wong_colors()[5], marker = :circle)]
-elem_4 = [MarkerElement(color = Makie.wong_colors()[2], marker = :utriangle)]
+elem_2 = [LineElement(color = :black, linestyle = :dash)]
+elem_3 = [MarkerElement(color = Makie.wong_colors()[5], marker = :circle, alpha=0.6)]
+elem_4 = [MarkerElement(color = Makie.wong_colors()[2], marker = :utriangle, alpha=0.6)]
 axislegend(ax, [elem_1, elem_2, elem_3, elem_4],
            ["theoretical stable", "theoretical unstable", "numerical stable", "numerical unstable"],
           position=:lt)
